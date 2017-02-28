3b007cf671cbad100daca44d1ac74f6aa1f76e52
Fred Wang <namelos@outlook.com>
2017-02-28 22:27:25 +0800

    replace stub model with database repo

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,7 +1,12 @@
 defmodule Rumbl.User do
-  @moduledoc """
-  In memory repository
-  """
+  use Rumbl.Web, :model
 
-  defstruct [:id, :name, :username, :password]
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
 end
