1baff58d057d54c96c1b114c1cb05fff57a6b733
Fred Wang <namelos@outlook.com>
2017-03-02 00:03:21 +0800

    login user after registration

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -21,11 +21,12 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
