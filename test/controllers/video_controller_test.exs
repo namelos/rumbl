91aa4997d2b9aba744ad0db8e37d08f08baef757
Fred Wang <namelos@outlook.com>
2017-03-04 22:04:30 +0800

    add invalid video creating test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -47,5 +47,13 @@ defmodule Rumbl.VideoControllerTest do
     assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
   end
 
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
+  end
+
   defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
 end
